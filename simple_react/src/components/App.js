34cde28215bb55a8b4a4c8395160809aa4ce05a5
Tabetha Walker-Smith <walkertasmith@gmail.com>
2017-07-20 08:18:24 -0700

    first commit

---
+++ simple_react/src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './';
+import './component/styles/App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="<div className="title">">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+
+<div className="my-header"=Earthquake!</div>
+
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+
+    );
+  }
+}
+
+export default App;
